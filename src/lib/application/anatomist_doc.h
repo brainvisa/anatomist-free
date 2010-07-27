83434d955e788fb126ce2b2ae27851344fa2efba
denisri <denis.riviere.gm@gmail.com>
2010-07-27 00:10:06 +0000

    a bit more doc on the OpenGL wrapping API in Anatomist
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/branches/4.0@38477 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/lib/application/anatomist_doc.h
+++ src/lib/application/anatomist_doc.h
@@ -64,6 +64,7 @@ French helps from Anatomist help menu:
 
 */
 
+
 /*! \page objects_opengl Anatomist Objects and OpenGL rendering
 
 Objects handled by Anatomist always inherit the anatomist::AObject class.
@@ -77,6 +78,7 @@ The main function of anatomist::GLComponent is to provide OpenGL display lists: 
 
 An object may have different representations in 3D, depending on viewing parameters, such as 2D/3D mode, slicing orientation, or other rendering parameters. This is why a rendering is always asked to an anatomist::GLComponent using a paremeter which describes which kind of rendering is needed: the anatomist::ViewState.
 
+
 \section multiple_gllists GLComponent generally contains multipls OpenGL display lists
 
 Anatomist tries to be minimalist when building 3D renderings: this means that whenever possible, display lists are shared between several views, or even between several objects when they have "something" in common. Moreover, when an object changes, anatomist tries to re-build only the part of the display lists that has actually changed. On an OpenGL point of view, this means that an object will generally hold several OpenGL display lists, each being responsible for a given aspect of the object rendering, and a main OpenGL list will call all the sub-lists. Typically, there will be an OpenGL list for color material, one for texture environment properties, one to build and bind the texture image(s), one for the geometric body (vertices and polygons)... This ends up with many lists, but allows to share efficiently things between views and objects, and to allow fast updates when the object changes.
@@ -87,9 +89,11 @@ This is why a GLComponent may be separated into its geometrical primitives, its 
 
 For each of these parts, state flags keep track of components which have changed, which will allow to re-calculate the modified lists when needed.
 
+
 \section glcomponent_refcounted_gllists OpenGL display lists are reference-counted
 
 Now it becomes obvious that a given OpenGL display list may be used in several objects and several views, and should be able to update when their underlying data have changed. This is why we use reference-counted GL lists.
+
 
 \section glcomponent_gllists_cache Display lists cache in GLComponent
 
@@ -98,11 +102,25 @@ A given display list can be built once, then reused for another view or object, 
 So GLComponent maintains a map of built display lists, indexed by a ViewState
 identifier, a string which describes characteristic information for a given anatomist::ViewState which are meaningful to the given object. This identifier is built by the anatomist::GLComponent::viewStateID() method. This GL lists map is a cache which allows lists to be reused. But its size should be limited, otherwise an object could grow and store thousands of display lists that may not be useful later. The cache maintains a given number of lists, and deletes the oldes which have not been used. This number of lists remembered in a given object can be queried and set by the anatomist::GLComponent::glStateMemory() and anatomist::GLComponent::glSetStateMemory() methods. anatomist::GLComponent::glGarbageCollector() may be used to manually free some lists, but normally is is done automatically.
 
+
 \section gllists_in_views OpenGL display lists in 3D views
 
 Anatomist 3D windows are two-layer:
 
 AWindow3D is the high-level object, contains the GUI, and manages AObject objects registration, removal, and updates after changes, and controls the whole scene. It has methods to completely redraw the scene, or perform partial recalculations. It makes use of OpenGL display lists of objects, and stores them in the lower-level view object: anatomist::GLWidgetManager.
 
-anatomist::GLWidgetManager is the low-level object. On the GUI point of view, it represents the 3D rendering zone, and stores all display lists to be rendered. It has no knowledge of which object a display list is related to, so cannot recalculate anything on its own after an object change. However it manages scene parameters, like camera position, perspective modes, scene snapshots etc.
+anatomist::GLWidgetManager is the low-level object. On the GUI point of view, it represents the 3D rendering zone, and stores all display lists to be rendered. It has no knowledge of which object a display list is related to, so cannot recalculate anything on its own after an object change. However it manages scene parameters, like camera position and orientation, perspective modes, scene snapshots etc.
+
+However if an object OpenGL display list(s) change, or if non-principal lists in an object are replaced with others, anatomist::GLWidgetManager state does not necessarily need to change, and a fast redraw can be triggered, the existing lists will just be called, and this is generally sufficient. More complete redraws implying objects bounding-box recalculations, adding/removing objects, or objects reordering, should be handled at the anatomist::Window3D level, because they need to change the structure of the principal lists.
+
+
+\section glcomponent_changing_contents Changing an object contents and updating its views
+
+Objects following the "standard" anatomist::GLComponent multiple display lists organization allow a quite general access to their geometric primitives, OpenGL lists, and "has-changed flags". This means that the vertice, normals, polygons and texture coordinates arrays can be accessed. This is done using the anatomist::GLComponent::glNumVertex(), anatomist::GLComponent::glVertexArray(), anatomist::GLComponent::glNormalArray(), anatomist::GLComponent::glPolygonSize(), anatomist::GLComponent::glNumPolygon(), anatomist::GLComponent::glPolygonArray(), anatomist::GLComponent::glNumTextures(), anatomist::GLComponent::glDimTex(), and anatomist::GLComponent::glTexCoordArray() methods, all using a anatomist::ViewState parameter to specify the desired rendering properties. For textrues, the min/max values are important, and texture images are normally associated with anatomist palettes.
+
+When an object changes, for instance if a vertex position has moved, or a texture coordinates value has changed, the corresponding anatomist::GLComponent::glSetChanged() method must be called, specifying which part of the object needs to be updated. Then the display lists will be recalculated if needed when needed, in a lazy way, and observers (views or parent objects) will also be updated consequently when the anatomist::AObject::notifyObservers() method is called.
+
+So basically, changing an object contents and updating anatomist views and objects does not require any OpenGL programming.
+
 */
+
