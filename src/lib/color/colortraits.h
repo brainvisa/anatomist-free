c0dc2e5692214b7da52ed745d88f4487fe9570c6
denisri <denis.riviere.gm@gmail.com>
2010-08-22 04:25:13 +0000

    use intensity of RGB images as opacity index in palettes
    #952
    
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/branches/4.0@38693 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/lib/color/colortraits.h
+++ src/lib/color/colortraits.h
@@ -293,7 +293,31 @@ namespace anatomist
     }
 
     col[2] = (*colors)( ival0, ival1 )[2];
-    col[3] = 255;
+
+    val = (float) ::sqrt( in.red() * in.red() + in.green() * in.green()
+        + in.blue() * in.blue() );
+
+    if( val <= minv0 )
+      ival0 = cmin0;
+    else if( val >= maxv0 )
+      ival0 = cmax0;
+    else
+      ival0 = (int)( scale0 * val + decal0 );
+
+    if( palette->palette1DMapping() == AObjectPalette::FIRSTLINE ||
+        colors->dimY() == 1 )
+      ival1 = 0 ;
+    else
+    {
+      if( val <= minv1 )
+        ival1 = cmin1;
+      else if( val >= maxv1 )
+        ival1 = cmax1;
+      else
+        ival1 = (int)( scale1 * val + decal1 );
+    }
+
+    col[3] = (*colors)( ival0, ival1 )[3];
 
     return col;
   }
