brainvisa/anatomist-free

commit 542b3bf4e09f26d603fc42dcb9975af7ecd66319

catch and handle pathologic case where the transformation property in a volume header is a single transform (not a list of transforms)
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/branches/4.1@43813 613412ce-1361-4b7a-93bf-a3c75af1bae4

src/lib/object/actions.cc
@@ -463,6 +463,22 @@ void ObjectActions::setAutomaticReferential( const set<AObject*> & obj )
           && ( transs = ps->getProperty( "transformations" ) )
           && refs.size() >= 1 && transs->size() >= 1 )
       {
+        // check that transformations is a list of trans and not a single trans
+        try
+        {
+          if( transs->size() != 0 )
+          {
+            Object it = transs->objectIterator();
+            it->size();
+          }
+        }
+        catch( runtime_error )
+        {
+          Object transs1 = transs;
+          transs = Object::value( vector<Object>( 1, transs ) );
+          cout << "Warning: non-conform transformations list in volume header "
+            "(single transformation)\n";
+        }
         Object      it;
         n = refs.size();
         // cout << "nifti transfo: " << n << endl;

commit 2c5a7646815860e2d7df348b739de4a145765091

oops, fixed a bug introduced in r43813
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/branches/4.1@43957 613412ce-1361-4b7a-93bf-a3c75af1bae4

src/lib/object/actions.cc
@@ -469,7 +469,18 @@ void ObjectActions::setAutomaticReferential( const set<AObject*> & obj )
           if( transs->size() != 0 )
           {
             Object it = transs->objectIterator();
-            it->size();
+            if( it->isValid() )
+            {
+              Object o = it->currentValue();
+              it = o->objectIterator();
+              if( !it->isValid() )
+              {
+                Object transs1 = transs;
+                transs = Object::value( vector<Object>( 1, transs ) );
+                cout << "Warning: non-conform transformations list in volume "
+                  "header (single transformation)\n";
+              }
+            }
           }
         }
         catch( runtime_error )