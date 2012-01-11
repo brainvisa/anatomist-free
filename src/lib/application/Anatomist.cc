3f155429370489157f2b658bd209f16ecf2c78a8
mreynal <mreynal@bioproj.invalid>
2012-01-11 13:26:35 +0000

    [Anatomist] - do not quit qApp when close anatomist mainWindow because qApp may be shared with brainvisa (if anatomist was imported using brainvisa.anatomist). qApp will be quit when lastWindowClosed() (thanks to bool quitOnLastWindowClosed default value is true)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/trunk@46302 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/lib/application/Anatomist.cc
+++ src/lib/application/Anatomist.cc
@@ -331,6 +331,9 @@ Anatomist::Anatomist( int argc, const char **argv,
   _privData->config = new GlobalConfiguration;
 
   _privData->paletteList = new PaletteList;
+//   qApp->setQuitOnLastWindowClosed(true);// default is already true
+//   cout<<"quitOnLastWindowClosed() (when true the qApp will be closed) : "<<QApplication::quitOnLastWindowClosed()<<endl;
+
 }
 
 
--- src/lib/control/wControl.cc
+++ src/lib/control/wControl.cc
@@ -1432,11 +1432,11 @@ void ControlWindow::graphParams()
 
 
 void ControlWindow::quit()
-{
-  qApp->quit();
+{  
+  // qApp->quit();// we don't want to quit the qapp here, because the qapp may be shared with brainvisa (if anatomist was imported using brainvisa.anatomist)
+  // don't worry, the qapp will be quit when lastWindowClosed() (because quitOnLastWindowClosed is true)
   //exit( EXIT_SUCCESS );
 }
-
 
 void ControlWindow::languageEnglish()
 {
