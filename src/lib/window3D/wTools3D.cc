8dffad37f0272b0da38592b050b1476f72910eb7
denisri <denis.riviere.gm@gmail.com>
2013-06-24 23:11:36 +0000

    some Qt3Support removal + allow clipping plane to be set more precisely (0.01mm)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/anatomist/anatomist-free/trunk@56647 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/lib/window3D/wTools3D.cc
+++ src/lib/window3D/wTools3D.cc
@@ -36,13 +36,10 @@
 #include <anatomist/window3D/window3D.h>
 #include <anatomist/window/glwidget.h>
 #include <qlayout.h>
-#include <aims/qtcompat/qvgroupbox.h>
 #include <qcheckbox.h>
-#include <aims/qtcompat/qvbuttongroup.h>
+#include <qbuttongroup.h>
 #include <qradiobutton.h>
-#include <aims/qtcompat/qhbox.h>
-#include <aims/qtcompat/qbuttongroup.h>
-#include <aims/qtcompat/qbutton.h>
+#include <qgroupbox.h>
 #include <qlabel.h>
 #include <qspinbox.h>
 #include <qtooltip.h>
@@ -55,14 +52,16 @@ struct Tools3DWindow::Private
 {
   Private() : destroying( false ) {}
 
-  QVButtonGroup	*renderfr;
+  QGroupBox	*renderfr;
+  QButtonGroup  *renderbg;
   QCheckBox	*pmode;
   QCheckBox	*transmode;
   QCheckBox	*cull;
   QCheckBox	*flatpoly;
   QCheckBox	*smooth;
   QCheckBox	*fog;
-  QVButtonGroup	*clip;
+  QGroupBox	*clip;
+  QButtonGroup  *clipbg;
   QSpinBox	*cld;
   bool		destroying;
   QCheckBox	*cursor;
@@ -72,36 +71,22 @@ struct Tools3DWindow::Private
 namespace
 {
 
-  void setButtonState( QButton* b, int x )
+  void setButtonState( QAbstractButton* b, int x )
   {
     QCheckBox	*cb = dynamic_cast<QCheckBox *>( b );
     if( !cb )
       return;
-#if QT_VERSION >= 0x040000
     switch( x )
-      {
-      case 0:
-        cb->setCheckState( Qt::Unchecked );
-        break;
-      case 1:
-        cb->setCheckState( Qt::Checked );
-        break;
-      default:
-        cb->setCheckState( Qt::PartiallyChecked );
-      }
-#else
-    switch( x )
-      {
-      case 0:
-        cb->setChecked( false );
-        break;
-      case 1:
-        cb->setChecked( true );
-        break;
-      default:
-        cb->setNoChange();
-      }
-#endif
+    {
+    case 0:
+      cb->setCheckState( Qt::Unchecked );
+      break;
+    case 1:
+      cb->setCheckState( Qt::Checked );
+      break;
+    default:
+      cb->setCheckState( Qt::PartiallyChecked );
+    }
   }
 
 }
@@ -116,157 +101,219 @@ Tools3DWindow::Tools3DWindow( AWindow3D *win )
   setAttribute(Qt::WA_DeleteOnClose);
   win->addObserver( this );
 
-  setCaption( tr( "3D settings : " ) + _window->Title().c_str() );
-  QVBoxLayout	*mainlay = new QVBoxLayout( this, 10, 5 );
+  setWindowTitle( tr( "3D settings : " ) + _window->Title().c_str() );
+  QVBoxLayout	*mainlay = new QVBoxLayout( this );
+  mainlay->setMargin( 10 );
+  mainlay->setSpacing( 5 );
 
-  QVGroupBox	*cubefr = new QVGroupBox( tr( "Helper gadgets :" ), this );
+  QGroupBox	*cubefr = new QGroupBox( tr( "Helper gadgets :" ), this );
+  QVBoxLayout *vlay = new QVBoxLayout( cubefr );
   d->cursor = new QCheckBox( tr( "Display cursor" ), cubefr );
+  vlay->addWidget( d->cursor );
   d->cursor->setTristate( true );
   setButtonState( d->cursor, win->hasSelfCursor() );
-  QToolTip::add( d->cursor, 
-                 tr( "Show/hide the linked cursor on the individual " 
-                     "associated window. You can override the global default "
-                     "(in the settings)" ) );
+  d->cursor->setToolTip(
+    tr( "<p align=\"justify\">Show/hide the linked cursor on the individual " 
+        "associated window. You can override the global default "
+        "(in the settings)</p>" ) );
   QCheckBox	*cubeEn = new QCheckBox( tr( "Display orientation cube" ), 
-					 cubefr );
+                                         cubefr );
+  vlay->addWidget( cubeEn );
   cubeEn->setChecked( _window->hasOrientationCube() );
   QCheckBox	*boundEn = new QCheckBox( tr( "Display bounding frame" ), 
-					  cubefr );
+                                          cubefr );
+  vlay->addWidget( boundEn );
   boundEn->setChecked( _window->hasBoundingFrame() );
   cubeEn->setEnabled( false );	// currently disabled (until we program it)
   boundEn->setEnabled( false );
+  vlay->addStretch( 1 );
 
-  QVButtonGroup	*renderfr = new QVButtonGroup( tr( "Rendering mode :" ), 
-					       this );
+  QGroupBox *renderfr = new QGroupBox( tr( "Rendering mode :" ), this );
+  vlay = new QVBoxLayout( renderfr );
+  QButtonGroup *renderbg = new QButtonGroup( renderfr );
+  d->renderbg = renderbg;
   d->renderfr = renderfr;
   QRadioButton	*r = new QRadioButton( tr( "Normal" ), renderfr );
-  QToolTip::add( r, tr( "Normal rendering mode - you should almost always\n" 
-			"use this one. It uses filled polygons") );
+  vlay->addWidget( r );
+  renderbg->addButton( r, 0 );
+  r->setToolTip( 
+    tr( "<p align=\"justify\">Normal rendering mode - you should almost "
+        "always use this one. It uses filled polygons</p>") );
   r = new QRadioButton( tr( "Wireframe" ), renderfr );
-  QToolTip::add( r, 
-		 tr( "Only polygon edges are drawn. Hidden faces are also\n" 
-		     "drawn, except for polygons facing back if culling\n" 
-		     "is enabled" ) );
+  vlay->addWidget( r );
+  renderbg->addButton( r, 1 );
+  r->setToolTip( 
+    tr( "<p align=\"justify\">Only polygon edges are drawn. Hidden faces are "
+        "also drawn, except for polygons facing back if culling " 
+        "is enabled</p>" ) );
   r = new QRadioButton( tr( "Outlined (filled faces + wireframes)" ), 
-			renderfr );
-  QToolTip::add( r, tr( "Polygons are filled and polygon edges are re-drawn\n" 
-			"using a different color (black). This mode is quite\n"
-			"slow as two renderings are performed" ) );
+                        renderfr );
+  vlay->addWidget( r );
+  renderbg->addButton( r, 2 );
+  r->setToolTip( 
+    tr( "<p align=\"justify\">Polygons are filled and polygon edges are "
+        "re-drawn using a different color (black). This mode is quite "
+        "slow as two renderings are performed</p>" ) );
   r = new QRadioButton( tr( "Wireframe with hidden faces" ), renderfr );
-  QToolTip::add( r, tr( "Only polygon edges are drawn, and hidden faces are\n" 
-			"actually hidden. To do so two renderings must be\n" 
-			"performed like in outline mode (slow)" ) );
+  vlay->addWidget( r );
+  renderbg->addButton( r, 3 );
+  r->setToolTip( 
+    tr( "<p align=\"justify\">Only polygon edges are drawn, and hidden faces "
+        "are actually hidden. To do so two renderings must be " 
+        "performed like in outline mode (slow)</p>" ) );
   QRadioButton	*rb = new QRadioButton( tr( "Fast rendering" ), renderfr );
-  QToolTip::add( rb, tr( "Well, this mode doesn't exist in fact...\n"
-			 "Maybe one day it will perform real-time polygon\n" 
-			 "pruning to speed-up huge mesh rendering,\n" 
-			 "but up to now..." ) );
-  renderfr->setButton( _window->renderingMode() );
+  vlay->addWidget( rb );
+  vlay->addStretch( 1 );
+  renderbg->addButton( rb, 4 );
+  rb->setToolTip( 
+    tr( "<p align=\"justify\">Well, this mode doesn't exist in fact... "
+        "Maybe one day it will perform real-time polygon " 
+        "pruning to speed-up huge mesh rendering, " 
+        "but up to now..." ) );
+  renderbg->button( _window->renderingMode() )->setChecked( true );
   rb->setEnabled( false );
 
   QCheckBox	*pmode = new QCheckBox( tr( "Use perspective" ), this );
   d->pmode = pmode;
   pmode->setChecked( _window->perspectiveEnabled() );
-  QToolTip::add( pmode, 
-		 tr( "Normal projection mode is orthogonal. In perspective\n" 
-		     "mode, you can get a distance effect between the\n" 
-		     "observer and objects. It is useful for the\n" 
-		     "'flight simulator' control." ) );
+  pmode->setToolTip(
+    tr( "<p align=\"justify\">Normal projection mode is orthogonal. In "
+        "perspective mode, you can get a distance effect between the " 
+        "observer and objects. It is useful for the " 
+        "'flight simulator' control.</p>" ) );
   QCheckBox	*transmode 
-    = new QCheckBox( tr( "Transparent objects are drawn in depth buffer" ), 
-		     this );
+    = new QCheckBox( tr( "Transparent objects are drawn in depth buffer" ),
+                     this );
   d->transmode = transmode;
-  QToolTip::add( transmode, 
-		 tr( "Enable this if you want to click on opaque objects\n" 
-		     "through transparent ones, or if some superimposed\n" 
-		     "transparent objects can't be seen. As a counterpart\n" 
-		     "objects superimposition will not be handled correctly" 
-		     ) );
+  transmode->setToolTip(
+    tr( "<p align=\"justify\">Enable this if you want to click on opaque "
+        "objects through transparent ones, or if some superimposed " 
+        "transparent objects can't be seen. As a counterpart " 
+        "objects superimposition will not be handled correctly</p>" 
+        ) );
   transmode->setChecked( _window->transparentZEnabled() );
   QCheckBox	*cull 
     = new QCheckBox( tr( "Cull polygon faces" ), this );
   d->cull = cull;
-  QToolTip::add( cull, 
-		 tr( "Culling avoids drawing polygons facing back.\n" 
-		     "Without this options, transparent objects will look\n" 
-		     "bad, but only the external side of open (or clipped)\n" 
-		     "meshes will be seen, so it depends on what you're\n" 
-		     "looking at" ) );
+  cull->setToolTip(
+    tr( "<p align=\"justify\">Culling avoids drawing polygons facing back. " 
+        "Without this options, transparent objects will look " 
+        "bad, but only the external side of open (or clipped) " 
+        "meshes will be seen, so it depends on what you're " 
+        "looking at</p>" ) );
   cull->setChecked( _window->cullingEnabled() );
   QCheckBox	*flatpoly 
     = new QCheckBox( tr( "Flat-shaded polygons" ), this );
   d->flatpoly = flatpoly;
-  QToolTip::add( flatpoly, 
-		 tr( "Normal rendering uses smoothed colors polygons, so you\n"
-		     "don't see polygon boundaries. It's nicer but if you\n" 
-		     "are looking at exact details of where polygons are,\n" 
-		     "you'll have to use either flat-shading, or a rendering\n"
-		     "mode showing polygon edges (wireframe, outline, ...)" 
-		     ) );
+  flatpoly->setToolTip(
+    tr( "<p align=\"justify\">Normal rendering uses smoothed colors "
+        "polygons, so you don't see polygon boundaries. It's nicer but if "
+        "you are looking at exact details of where polygons are, " 
+        "you'll have to use either flat-shading, or a rendering "
+        "mode showing polygon edges (wireframe, outline, ...)</p>" 
+        ) );
   flatpoly->setChecked( _window->flatShading() );
   QCheckBox	*smooth 
     = new QCheckBox( tr( "Smooth polygons / lines" ), this );
   d->smooth = smooth;
-  QToolTip::add( smooth, 
-		 tr( "This enables smooth filtering (anti-aliasing) of lines\n"
-		     "and polygons edges, but doesn't work well in all\n" 
-		     "cases (polygons should be sorted by depth), and it can\n"
-		     "be very slow" ) );
+  smooth->setToolTip(
+    tr( "<p align=\"justify\">This enables smooth filtering (anti-aliasing) "
+        "of lines and polygons edges, but doesn't work well in all " 
+        "cases (polygons should be sorted by depth), and it can "
+        "be very slow</p>" ) );
   smooth->setChecked( _window->smoothing() );
   QCheckBox	*fog = new QCheckBox( tr( "Fog" ), this );
   d->fog = fog;
-  QToolTip::add( fog, tr( "Use fog (useful only in perspective mode" ) );
+  fog->setToolTip( tr( "Use fog (useful only in perspective mode" ) );
   d->fog->setChecked( _window->fog() );
 
-  QVButtonGroup	*clip = new QVButtonGroup( tr( "Clipping plane :" ), this );
+  QGroupBox *clip = new QGroupBox( tr( "Clipping plane :" ), this );
+  vlay = new QVBoxLayout( clip );
+  QButtonGroup *clipbg = new QButtonGroup( clip );
+  d->clipbg = clipbg;
   d->clip = clip;
-  new QRadioButton( tr( "No clipping" ), clip );
+  r = new QRadioButton( tr( "No clipping" ), clip );
+  vlay->addWidget( r );
+  clipbg->addButton( r, 0 );
   r = new QRadioButton( tr( "Single plane" ), clip );
-  QToolTip::add( r, 
-		 tr( "You can clip the whole scene to cut meshes on one or\n" 
-		     "both sides of the current plane of the 2D window mode" 
-		     ) );
-  new QRadioButton( tr( "Clip both sides" ), clip );
-  clip->setButton( (int) _window->clipMode() );
-  QHBox		*clb = new QHBox( clip );
-  clb->setSpacing( 10 );
-  QLabel	*l 
-    = new QLabel( tr( "Distance to current plane (mm) :" ), clb );
-  QToolTip::add( l, 
-		 tr( "This is the thickness of the slice between the current\n"
-		     "2D window plane and the clipping plane(s). If you are\n" 
-		     "using a volume (slice), don't set the distance to zero\n"
-		     "because the slice will be just on the clipping plane,\n" 
-		     "and you don't know what will be drawn and what won't be" 
-		     ) );
+  vlay->addWidget( r );
+  clipbg->addButton( r, 1 );
+  r->setToolTip( 
+    tr( "<p align=\"justify\">You can clip the whole scene to cut meshes on "
+        "one or both sides of the current plane of the 2D window mode</p>"
+      ) );
+  r = new QRadioButton( tr( "Clip both sides" ), clip );
+  vlay->addWidget( r );
+  clipbg->addButton( r, 2 );
+  clipbg->button( (int) _window->clipMode() )->setChecked( true );
+  QWidget *clb = new QWidget( clip );
+  vlay->addWidget( clb );
+  QHBoxLayout *hlay = new QHBoxLayout( clb );
+  hlay->setSpacing( 10 );
+  hlay->setMargin( 0 );
+  QLabel *l = new QLabel( tr( "Distance to current plane (mm/100) :" ), 
+                          clb );
+  hlay->addWidget( l );
+  l->setToolTip( 
+    tr( "<p align=\"justify\">This is the thickness of the slice between the "
+        "current 2D window plane and the clipping plane(s). If you are " 
+        "using a volume (slice), don't set the distance to zero "
+        "because the slice will be just on the clipping plane, " 
+        "and you don't know what will be drawn and what won't be</p>" 
+      ) );
   QSpinBox	*cld = new QSpinBox( clb );
+  hlay->addWidget( cld );
   d->cld = cld;
-  cld->setValue( (int) _window->clipDistance() );
+  cld->setRange( 0, 100000 );
+  cld->setSingleStep( 10 );
+  cld->setValue( (int) ( _window->clipDistance() * 100 ) );
+  vlay->addStretch( 1 );
 
   // depth peeling tag
-  QVButtonGroup	*dpg = new QVButtonGroup( tr( "Transparency improvement:" ), 
-					  this );
-  QCheckBox	*dpeel = new QCheckBox( tr( "Use slower-but-better "
-					    "transparency rendering algorithm" 
-					    ), dpg );
-  QHBox		*dppb = new QHBox( dpg );
-  dppb->setSpacing( 10 );
-  new QLabel( tr( "Number of rendering passes (0=as necessary)" ), dppb );
+  QGroupBox *dpg = new QGroupBox( tr( "Transparency improvement:" ), this );
+  vlay = new QVBoxLayout( dpg );
+  QCheckBox *dpeel = new QCheckBox( tr( "Use slower-but-better "
+                                        "transparency rendering algorithm" 
+                                      ), dpg );
+  vlay->addWidget( dpeel );
+  QWidget *dppb = new QWidget( dpg );
+  vlay->addWidget( dppb );
+  hlay = new QHBoxLayout( dppb );
+  hlay->setSpacing( 10 );
+  hlay->setMargin( 0 );
+  hlay->addWidget( new QLabel( tr( 
+    "Number of rendering passes (0=as necessary)" ), dppb ) );
   QSpinBox	*dppass = new QSpinBox( dppb );
+  hlay->addWidget( dppass );
 
   GLWidgetManager	*da = static_cast<GLWidgetManager *>( win->view() );
   if( !da->depthPeelingAllowed() )
     dpg->setEnabled( false );
   dpeel->setChecked( da->depthPeelingEnabled() );
   dppass->setValue( (int) da->depthPeelingPasses() );
+  vlay->addStretch( 1 );
 
   // window saving modes
-  QVButtonGroup	*wsg = new QVButtonGroup( tr( "Window saving modes:" ), this );
+  QGroupBox	*wsg = new QGroupBox( tr( "Window saving modes:" ), this );
+  vlay = new QVBoxLayout( wsg );
+  QButtonGroup *wsgg = new QButtonGroup( wsg );
+  wsgg->setExclusive( false );
   QCheckBox	*wsrgb = new QCheckBox( tr( "RGB buffer" ), wsg );
+  vlay->addWidget( wsrgb );
+  wsgg->addButton( wsrgb, 0 );
   QCheckBox	*wsa = new QCheckBox( tr( "Alpha buffer" ), wsg );
+  vlay->addWidget( wsa );
+  wsgg->addButton( wsa, 1 );
   QCheckBox	*wsrgba = new QCheckBox( tr( "RGBA buffer" ), wsg );
+  vlay->addWidget( wsrgba );
+  wsgg->addButton( wsrgba, 2 );
   QCheckBox	*wsd = new QCheckBox( tr( "Depth buffer" ), wsg );
+  vlay->addWidget( wsd );
+  wsgg->addButton( wsd, 3 );
   QCheckBox	*wsl = new QCheckBox( tr( "Luminance buffer" ), wsg );
+  vlay->addWidget( wsl );
+  wsgg->addButton( wsl, 4 );
+  vlay->addStretch( 1 );
 
   int	savem = da->otherBuffersSaveMode();
   wsrgb->setChecked( savem & 1 );
@@ -295,11 +342,12 @@ Tools3DWindow::Tools3DWindow( AWindow3D *win )
 	   SLOT( enableCube( bool ) ) );
   connect( boundEn, SIGNAL( toggled( bool ) ), this, 
 	   SLOT( enableBoundingFrame( bool ) ) );
-  connect( renderfr, SIGNAL( clicked( int ) ), this, 
+  connect( renderbg, SIGNAL( buttonClicked( int ) ), this, 
 	   SLOT( setRenderMode( int ) ) );
   connect( pmode, SIGNAL( toggled( bool ) ), this, 
 	   SLOT( enablePerspective( bool ) ) );
-  connect( clip, SIGNAL( clicked( int ) ), this, SLOT( setClipMode( int ) ) );
+  connect( clipbg, SIGNAL( buttonClicked( int ) ), 
+           this, SLOT( setClipMode( int ) ) );
   connect( cld, SIGNAL( valueChanged( int ) ), this, 
 	   SLOT( setClipDistance( int ) ) );
   connect( transmode, SIGNAL( toggled( bool ) ), this, 
@@ -314,7 +362,7 @@ Tools3DWindow::Tools3DWindow( AWindow3D *win )
 	   SLOT( enableDepthPeeling( bool ) ) );
   connect( dppass, SIGNAL( valueChanged( int ) ), this, 
 	   SLOT( setDepthPeelingPasses( int ) ) );
-  connect( wsg, SIGNAL( clicked( int ) ), this, 
+  connect( wsgg, SIGNAL( buttonClicked( int ) ), this, 
 	   SLOT( toggleSavingMode( int ) ) );
 }
 
@@ -332,7 +380,7 @@ Tools3DWindow::~Tools3DWindow()
 
 void Tools3DWindow::update( const Observable*, void* arg )
 {
-  cout << "Tools3DWindow::update\n";
+  // cout << "Tools3DWindow::update\n";
 
   if( arg == 0 )
     {
@@ -343,14 +391,14 @@ void Tools3DWindow::update( const Observable*, void* arg )
 
   blockSignals( true );
 
-  d->renderfr->setButton( _window->renderingMode() );
+  d->renderbg->button( _window->renderingMode() )->setChecked( true );
   d->pmode->setChecked( _window->perspectiveEnabled() );
   d->transmode->setChecked( _window->transparentZEnabled() );
   d->cull->setChecked( _window->cullingEnabled() );
   d->flatpoly->setChecked( _window->flatShading() );
   d->smooth->setChecked( _window->smoothing() );
-  d->clip->setButton( (int) _window->clipMode() );
-  d->cld->setValue( (int) _window->clipDistance() );
+  d->clipbg->button( (int) _window->clipMode() )->setChecked( true );
+  d->cld->setValue( (int) ( _window->clipDistance() * 100 ) );
   d->smooth->setChecked( _window->smoothing() );
   setButtonState( d->cursor, _window->hasSelfCursor() );
 
@@ -410,7 +458,7 @@ void Tools3DWindow::setClipMode( int m )
 
 void Tools3DWindow::setClipDistance( int d )
 {
-  _window->setClipDistance( (float) d );
+  _window->setClipDistance( (float) d * 0.01 );
   _window->Refresh();
   ( (GLWidgetManager *) _window->view() )->copyBackBuffer2Texture();
 }
@@ -467,12 +515,10 @@ void Tools3DWindow::setDepthPeelingPasses( int n )
 
 void Tools3DWindow::toggleSavingMode( int x )
 {
-  cout << "toggleSavingMode " << x << endl;
   GLWidgetManager *da = static_cast<GLWidgetManager *>( _window->view() );
   int	savem = da->otherBuffersSaveMode();
   savem ^= ( 1 << x );
   da->setOtherBuffersSaveMode( savem );
-  cout << "new saving mode: " << savem << endl;
 }
 
 
@@ -480,10 +526,10 @@ void Tools3DWindow::setCursorVisibility( int x )
 {
   switch( x )
     {
-    case QCheckBox::On:
+    case Qt::Checked:
       x = 1;
       break;
-    case QCheckBox::Off:
+    case Qt::Unchecked:
       x = 0;
       break;
     default:
