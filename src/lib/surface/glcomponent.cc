5fb1b2dcd355e04c52465e0cd08adec9c0582cc3
Denis Rivière <denis.riviere.gm@gmail.com>
2025-10-01 11:17:49 +0200

    removed duplicated settings

--- src/lib/surface/glcomponent.cc
+++ src/lib/surface/glcomponent.cc
@@ -1446,7 +1446,7 @@ bool GLComponent::glMakeTexEnvGLL( const ViewState & state,
           glDisable( GL_TEXTURE_2D );
           glDisable( GL_TEXTURE_3D );
         }
-      if( glTexRGBInterpolation( tex ) )
+      if( rgb )
         glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
       else
         glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, glGLTexMode( tex ) );
@@ -1467,11 +1467,6 @@ bool GLComponent::glMakeTexEnvGLL( const ViewState & state,
                    && ( tm == glTEX_REFLECTION_MAP 
                         || tm == glTEX_NORMAL_MAP ) ) )
         tm = glTEX_MANUAL;
-
-      if( rgb )
-        glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-      else
-        glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, glGLTexMode( tex ) );
 
       switch( tm )
         {
